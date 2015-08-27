5513119acfc7981713d30269254134455806dffc
filchakov <filchakov.denis@gmail.com>
2015-08-27 15:47:13 +0300

    [+] regex and change structure

--- ruby_test.rb
+++ ruby_test.rb
@@ -1,14 +1,16 @@
 #!/usr/bin/env ruby
+require 'date'
 
-type_report = ['account', 'activity', 'position', 'security']
-
+allow_type_report = ['account', 'activity', 'position', 'security']
 tree = Hash.new {|hash, key| hash[key] = Hash.new(&hash.default_proc)}
 
 Dir.foreach(".") do |f|
+	next unless f.match(/^*_*_.*(txt)/)
 	info_file = f.capitalize.split('_')
-	if type_report.include? info_file[1]
-		tree[info_file[0]][info_file[1]] = [] if tree[info_file[0]][info_file[1]] == {}
-		tree[info_file[0]][info_file[1]].push(info_file[2]).sort!
+	account, type_report, date = info_file[0], info_file[1], Date.parse(File.basename(info_file[2].to_s, ".*").to_s).strftime("%Y-%m-%d")
+	if allow_type_report.include? type_report
+		tree[account][date] = [] if tree[account][date] == {}
+		tree[account][date].push(type_report).sort!
 	end
 end
 
