79170f91125f1595c5a57d19e14ca2d9c1d32a3d
filchakov <filchakov.denis@gmail.com>
2015-08-27 17:46:16 +0300

    fix sort by date

--- ruby_test.rb
+++ ruby_test.rb
@@ -1,17 +1,21 @@
 #!/usr/bin/env ruby
 require 'date'
+require 'json'
 
-allow_type_report = ['account', 'activity', 'position', 'security']
+ALLOW_TYPE_REPORT = ['account', 'activity', 'position', 'security']
+DATE_FORMAT = "%Y-%m-%d"
+
 tree = Hash.new {|hash, key| hash[key] = Hash.new(&hash.default_proc)}
 
-Dir.foreach(".") do |f|
+Dir.glob("*txt").sort.each { |f|
 	next unless f.match(/^*_*_.*(txt)/)
 	info_file = f.capitalize.split('_')
-	account, type_report, date = info_file[0], info_file[1], Date.parse(File.basename(info_file[2].to_s, ".*").to_s).strftime("%Y-%m-%d")
-	if allow_type_report.include? type_report
+	account, type_report, date = info_file[0], info_file[1], Date.parse(File.basename(info_file[2].to_s, ".*").to_s).strftime(DATE_FORMAT)
+	
+	if ALLOW_TYPE_REPORT.include? type_report
 		tree[account][date] = [] if tree[account][date] == {}
 		tree[account][date].push(type_report).sort!
 	end
-end
+}
 
-puts tree
+puts tree.to_json
